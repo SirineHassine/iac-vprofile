918e8fc09ee102a9f6ecf12306a1832b3aa8d026
sirine.hassine14@gmail.com <sirine.hassine14@gmail.com>
2024-10-13 17:57:03 +0200

    add action workflow

--- terraform/vpc.tf
+++ terraform/vpc.tf
@@ -14,6 +14,7 @@ module "vpc" {
   single_nat_gateway   = true
   enable_dns_hostnames = true
 
+
   public_subnet_tags = {
     "kubernetes.io/cluster/${local.cluster_name}" = "shared"
     "kubernetes.io/role/elb"                      = 1
